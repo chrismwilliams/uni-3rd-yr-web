e32aca967440878255cf515c1b8ae48505b2e15b
Chris Williams <christophw+github@protonmail.com>
2018-12-05 11:00:55 +0000

    rm head from master; update map api config

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,18 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Find your next favourite course with coursefinder">
-  <title>coursefinder - @yield('title')</title>
-  <link href="https://fonts.googleapis.com/css?family=Cantata+One|Raleway:500" rel="stylesheet">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
-  
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-</head>
+@include('partials.header')
 <body>
   <div id="app" class="grid">
     @include ('partials.nav')
@@ -22,7 +10,7 @@
     </div>
     @include ('partials.footer')
   </div>
-  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{config('app.google_api')}}&libraries=places"></script>
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key={{config('services.google_api')}}&libraries=places"></script>
   
   <script src="{{ mix('js/manifest.js') }}"></script>
   <script src="{{ mix('js/vendor.js') }}"></script>
