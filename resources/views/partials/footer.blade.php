7779fa1c0c0906fee2275e00829e210adaf8da2c
Chris Williams <christophw+github@protonmail.com>
2018-12-05 11:22:37 +0000

    rm social links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,30 +1,10 @@
 <footer>
-  <div class="columns level is-mobile is-centered">
-    <div class="column"></div>
-    <div class="site_name column has-text-centered"><span>coursefinder</span>&copy;&nbsp;<a href="https://www.chriswilliams.co" target="_blank">chris williams</a> </div>
-    <div class="social column has-text-left">
-      <ul class="social">
-        <li>
-          <a href="https://www.facebook.com" target="_blank">
-            <i class="fab fa-facebook-square" aria-hidden="true"></i>
-            Facebook
-          </a>
-        </li>
-  
-        <li>
-          <a href="https://www.twitter.com" target="_blank">
-            <i class="fab fa-twitter-square" aria-hidden="true"></i>
-            Twitter
-          </a>
-        </li>
-  
-        <li>
-          <a href="https://www.instagram.com" target="_blank">
-            <i class="fab fa-instagram" aria-hidden="true"></i>
-            Instagram
-          </a>
-        </li>
-      </ul>
+  <div class="columns is-mobile is-centered">
+    <div class="site_name column has-text-centered">
+      <span>coursefinder</span>
+      <span class="is-flex">&copy;&nbsp;
+        <a href="https://www.chriswilliams.co" target="_blank" rel="nofollow noopener noreferrer">chris williams</a>
+      </span>
     </div>
   </div>
 </footer>
